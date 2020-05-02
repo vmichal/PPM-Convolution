116f2f292b2f35d99405acfaa295a090bf2ae9e8
Vojtěch Michal <vojta.michall@email.cz>
2020-05-02 02:57:13 +0200

    12.8 points gained by using curlybraces to call destructors early.

--- main.cpp
+++ main.cpp
@@ -39,11 +39,11 @@ struct pixel {
 
 static_assert(sizeof(pixel) == 3);
 
-int clamp(int min, int current, int max) {
-	return current < min ? min : current > max ? max : current;
+constexpr int clamp(int current) {
+	return current > 255 ? 255 : current < 0 ? 0 : current;
 }
 
-int greyscale(pixel const p) {
+constexpr int greyscale(pixel const p) {
 	return std::round(p.r * 0.2126 + p.g * 0.7152 + p.b * 0.0722);
 }
 
@@ -75,10 +75,11 @@ int main(int argc, char** argv) {
 	input.ignore(1); //ignore the last newline after header
 	int const width = W, height = H;
 
+	int const size = width * height;
 
-	std::vector<pixel> pixels(height * width);
-	std::vector<pixel> output_buffer(height * width);
-	input.read(reinterpret_cast<char*>(pixels.data()), height * width * sizeof(pixel));
+	std::vector<pixel> pixels(size);
+	std::vector<pixel> output_buffer(size);
+	input.read(reinterpret_cast<char*>(pixels.data()), size * sizeof(pixel));
 
 	std::vector<pixel const*> lines(height);
 	for (int i = 0; i < height; ++i)
@@ -106,9 +107,9 @@ int main(int argc, char** argv) {
 
 		for (int col = 1; col < width - 1; ++col) {
 
-			uint8_t const red = clamp(0, 5 * current[col].r - current[col - 1].r - current[col + 1].r - previous[col].r - next[col].r, 255);
-			uint8_t const green = clamp(0, 5 * current[col].g - current[col - 1].g - current[col + 1].g - previous[col].g - next[col].g, 255);
-			uint8_t const blue = clamp(0, 5 * current[col].b - current[col - 1].b - current[col + 1].b - previous[col].b - next[col].b, 255);
+			uint8_t const red = clamp(5 * current[col].r - current[col - 1].r - current[col + 1].r - previous[col].r - next[col].r);
+			uint8_t const green = clamp(5 * current[col].g - current[col - 1].g - current[col + 1].g - previous[col].g - next[col].g);
+			uint8_t const blue = clamp(5 * current[col].b - current[col - 1].b - current[col + 1].b - previous[col].b - next[col].b);
 
 			finalize_pixel({ red, green, blue });
 		}
@@ -119,19 +120,24 @@ int main(int argc, char** argv) {
 	for (int i = 0; i < width; ++i)
 		finalize_pixel(lines[height - 1][i]);
 
-	histogram[4] += histogram[5]; //accounting for the hacky histogram computaion
+	histogram[4] += histogram[5]; //accounting for the hacky histogram computation
 
-	std::ostringstream output_header;
+	{
+		std::ostringstream output_header;
+		output_header << file_magic << '\n' << width << '\n' << height << '\n' << max_value << '\n';
+		std::string const header = output_header.str();
 
-	output_header << file_magic << '\n' << width << '\n' << height << '\n' << max_value << '\n';
-	std::string const header = output_header.str();
-	std::ofstream output("output.ppm", std::ios::binary);
-	output << header;
-	output.write(reinterpret_cast<char*>(output_buffer.data()), width * height * sizeof(pixel));
-	std::ofstream histogram_output("output.txt");
-	std::copy(histogram.begin(), histogram.begin() + 4, std::ostream_iterator<int>(histogram_output, " "));
-	histogram_output << histogram[4];
+		std::ofstream output("output.ppm", std::ios::binary);
+		output << header;
+		output.write(reinterpret_cast<char*>(output_buffer.data()), size * sizeof(pixel));
+	}
 
+	{
+		std::ofstream histogram_output("output.txt");
+		std::copy(histogram.begin(), histogram.begin() + 4, std::ostream_iterator<int>(histogram_output, " "));
+		histogram_output << histogram[4];
+
+	}
 	return 0;
 
 }
