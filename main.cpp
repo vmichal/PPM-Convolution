536853c873b5161c0e6447f61c5442d006e23bd5
Vojtěch Michal <vojta.michall@email.cz>
2020-05-02 02:09:49 +0200

    Naive solution. One point with cost almost 9.
    Slowness is caused by direct output into the file. Will fix by buffering.
    
    ==37== Cachegrind, a cache and branch-prediction profiler
    ==37== Copyright (C) 2002-2017, and GNU GPL'd, by Nicholas Nethercote et
    al.
    ==37== Using Valgrind-3.14.0 and LibVEX; rerun with -h for copyright
    info
    ==37== Command: ./sub_main vit_normal.ppm
    ==37==
    --37-- warning: L3 cache found, using its data for the LL simulation.
    ==37==
    ==37== I   refs:      9,653,157,367
    ==37== I1  misses:            2,184
    ==37== LLi misses:            2,055
    ==37== I1  miss rate:          0.00%
    ==37== LLi miss rate:          0.00%
    ==37==
    ==37== D   refs:      4,762,601,870  (3,233,351,422 rd   + 1,529,250,448
    wr)
    ==37== D1  misses:        3,349,274  (    2,305,088 rd   +     1,044,186
    wr)
    ==37== LLd misses:        1,505,554  (      755,805 rd   +       749,749
    wr)
    ==37== D1  miss rate:           0.1% (          0.1%     +
    0.1%  )
    ==37== LLd miss rate:           0.0% (          0.0%     +
    0.0%  )
    ==37==
    ==37== LL refs:           3,351,458  (    2,307,272 rd   +     1,044,186
    wr)
    ==37== LL misses:         1,507,609  (      757,860 rd   +       749,749
    wr)
    ==37== LL miss rate:            0.0% (          0.0%     +
    0.0%  )
    Ir	9653157367
    I1mr	2184
    ILmr	2055
    Dr	3233351422
    D1mr	2305088
    DLmr	755805
    Dw	1529250448
    D1mw	1044186
    DLmw	749749
    AMAT_d	0.649425
    AMAT_i	0.600032
    Cost	8.88516
    OK
    Points: 1.0

---
+++ main.cpp
@@ -0,0 +1,136 @@
+//Homework assignment for university course Computer architectures at CTU FEE
+//https://cw.fel.cvut.cz/wiki/courses/b35apo/homeworks/02/start
+
+#pragma GCC optimize("Ofast")
+
+#include <iostream>
+#include <iterator>
+#include <array>
+#include <algorithm>
+#include <vector>
+#include <cassert>
+#include <fstream>
+#include <string>
+#include <cstdint>
+#include <sstream>
+#include <cmath>
+#include <numeric>
+
+constexpr auto file_magic = "P6";
+
+
+int print_help(std::string program_name) {
+	std::cerr <<
+		"Usage: " << program_name << " argument\n"
+		"Argument shall be a valid path to ppm picture.\n";
+
+	return 0;
+}
+
+bool check_magic(std::ifstream& input) {
+	std::string line;
+	std::getline(input, line);
+	return line == file_magic;
+}
+
+struct pixel {
+	std::uint8_t r, g, b;
+};
+std::ostream& operator<<(std::ostream& stream, pixel const p) {
+	return stream << p.r << p.g << p.b;
+}
+std::ostream& print_pixel(std::ostream& stream, pixel const p) {
+	return stream << '[' << +p.r << ", " << +p.g << ", " << +p.b << ']';
+}
+
+static_assert(sizeof(pixel) == 3);
+
+int clamp(int min, int current, int max) {
+	return current < min ? min : current > max ? max : current;
+}
+
+int greyscale(pixel const p) {
+	return std::round(p.r * 0.2126 + p.g * 0.7152 + p.b * 0.0722);
+}
+
+
+int main(int argc, char** argv) {
+
+
+	if (argc != 2) {
+		std::cout << "Incorrect number of arguments.\n";
+		return print_help(argv[0]);
+	}
+
+	std::ifstream input(argv[1]);
+
+	if (!input.is_open() || !check_magic(input)) {
+		std::cerr << "This is not a ppm file!\n";
+		return print_help(argv[0]);
+	}
+	if (input.peek() == '#') {
+		std::string comment; //Extract single comment line from input file
+		std::getline(input, comment);
+	}
+
+
+	int width, height, max_value;
+	input >> width >> height >> max_value;
+	assert(width > 0 && height > 0 && max_value == 255);
+	input.ignore(1); //ignore the last newline after header
+
+	std::ofstream output("output.ppm", std::ios::binary);
+	std::ostringstream output_header;
+
+	output_header << file_magic << '\n' << width << '\n' << height << '\n' << max_value << '\n';
+	std::string const header = output_header.str();
+	output << header;
+
+	std::vector<pixel> pixels(height * width);
+	input.read(reinterpret_cast<char*>(pixels.data()), height * width * sizeof(pixel));
+
+	std::vector<pixel*> lines(height);
+	for (int i = 0; i < height; ++i)
+		lines[i] = pixels.data() + width * i;
+
+
+	std::array<int, 6> histogram{ {0} };
+
+	auto const finalize_pixel = [&](pixel const pixel) {
+		uint8_t const grey = greyscale(pixel);
+
+		histogram[grey / 51]++;
+		output << pixel;
+	};
+
+	for (int i = 0; i < width; ++i)
+		finalize_pixel(lines[0][i]);
+
+	for (int row = 1; row < height - 1; ++row) {
+		finalize_pixel(lines[row][0]);
+
+		pixel const* const current = lines[row], * const previous = lines[row - 1], * const next = lines[row + 1];
+
+		for (int col = 1; col < width - 1; ++col) {
+
+			uint8_t const red = clamp(0, 5 * current[col].r - current[col - 1].r - current[col + 1].r - previous[col].r - next[col].r, 255);
+			uint8_t const green = clamp(0, 5 * current[col].g - current[col - 1].g - current[col + 1].g - previous[col].g - next[col].g, 255);
+			uint8_t const blue = clamp(0, 5 * current[col].b - current[col - 1].b - current[col + 1].b - previous[col].b - next[col].b, 255);
+
+			finalize_pixel({ red, green, blue });
+		}
+
+		finalize_pixel(lines[row][width - 1]);
+	}
+
+	for (int i = 0; i < width; ++i)
+		finalize_pixel(lines[height - 1][i]);
+
+	histogram[4] += histogram[5]; //accounting for the hacky histogram computaion
+
+	std::ofstream histogram_output("output.txt");
+	std::copy(histogram.begin(), histogram.end()-1, std::ostream_iterator<int>(histogram_output, " "));
+
+	return 0;
+
+}
